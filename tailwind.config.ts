67209b87f79628ab208784ed786fac0b163c0016
rizkianakbar <rizkianakbar4@gmail.com>
2024-10-06 06:56:21 +0700

    chore: add tailwind colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,8 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: "#1E2125",
+        primary: "#579DFF",
       },
     },
   },
