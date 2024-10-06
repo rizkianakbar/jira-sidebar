4f17fb8aa8a45a8d2a6f142da654cdedbf711a56
rizkianakbar <rizkianakbar4@gmail.com>
2024-10-06 22:59:24 +0700

    fix: remove _animateMain func because it makes the resizer laggy

--- components/sidebar/sidebar.hooks.ts
+++ components/sidebar/sidebar.hooks.ts
@@ -15,13 +15,11 @@ const useSidebar = (
     setSidebarData(newSidebarData);
     _putWidth(SIDEBAR.SIZE_DEFAULT);
     await _removeAnimable();
-    _animateMain(SIDEBAR.SIZE_DEFAULT);
   };
 
   const closeSidebar = async () => {
     const newSidebarData = { isSidebarOpen: false, size: SIDEBAR.SIZE_MIN };
     _stopResize();
-    _animateMain(20);
 
     setSidebarData(newSidebarData);
     await _addAnimable();
@@ -46,7 +44,6 @@ const useSidebar = (
     if (newWidth > SIDEBAR.SIZE_MAX) newWidth = SIDEBAR.SIZE_MAX;
 
     _putWidth(newWidth);
-    _animateMain(newWidth);
     setSidebarData((prevSidebarData) => {
       const updatedSidebarData = { ...prevSidebarData, size: newWidth };
       return updatedSidebarData;
@@ -56,14 +53,6 @@ const useSidebar = (
   const _putWidth = (newWidth: number) => {
     if (!sidebarRef || !sidebarRef.current) return;
     sidebarRef.current.style.width = `${newWidth}px`;
-  };
-
-  const _animateMain = (newWidth: number) => {
-    const mainElement = document.getElementById("main");
-    if (mainElement) {
-      mainElement.style.transition = "margin-left 0.2s ease-in-out";
-      mainElement.style.marginLeft = `${newWidth}px`;
-    }
   };
 
   const _addAnimable = (): Promise<void> => {
